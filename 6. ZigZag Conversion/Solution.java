17ca46cc4a5cd6136e54ee66771afefa42742405
goodtecher <goodtechllc@gmail.com>
2017-12-02 14:34:21 -0500

    6. ZigZag Conversion

---
+++ 6. ZigZag Conversion/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * GoodTecher LeetCode Tutorial 6. ZigZag Conversion
+ *
+ * http://www.goodtecher.com/leetcode-6-zigzag-conversion-java/
+ *
+ * http://www.goodtecher.com
+ *
+ * https://github.com/GoodTecher/
+ */
+
+class Solution {
+  public String convert(String s, int numRows) {
+    int length = s.length();
+
+    if (numRows > length || numRows <= 1) {
+      return s;
+    }
+
+    char[] zigZagChars = new char[length];
+    int count = 0;
+
+    int interval = 2 * numRows - 2;
+
+    for (int i = 0; i < numRows; i++) {
+      int step = interval - 2 * i;
+      for (int j = i; j < length; j += interval) {
+        zigZagChars[count] = s.charAt(j);
+        count++;
+        if (step > 0 && step < interval && j + step < length) {
+          zigZagChars[count] = s.charAt(j + step);
+          count++;
+        }
+      }
+    }
+
+    return new String(zigZagChars);
+  }
+}
